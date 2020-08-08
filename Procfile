c8902a90d1d5ef554ca3115c3c8801612c440e89
brown5628 <brown5628@gmail.com>
2020-08-08 07:34:05 -0400

    Stub streamlit app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: streamlit run agile_metrics.py
---
+++ agile_metrics.py
@@ -0,0 +1,14 @@
+# %%
+import streamlit as st
+import pandas as pd
+import altair as alt
+import psycopg2
+import os
+
+# %%
+def main():
+    st.balloons()
+
+
+if __name__ == "__main__":
+    main()

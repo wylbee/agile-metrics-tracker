c590baef793d2b625f3ba53565329fe6a4de5e57
brown5628 <brown5628@gmail.com>
2020-08-15 09:44:16 -0400

    Add Lead Time & related metrics

--- agile_metrics.py
+++ agile_metrics.py
@@ -42,6 +42,18 @@ df = create_df_from_query(
 df['date_hour'] = pd.to_datetime(df['date_hour'])
 
 
+df_cfd = create_df_from_query(
+    """
+    select
+        *
+    from dev_wbrown.snap_kanban_daily_flow
+
+    where date_day >= '2020-07-06'
+    """
+)
+
+df_cfd['date_day'] = pd.to_datetime(df_cfd['date_day'])
+
 # Set theme
 alt.themes.enable('latimes')
 
@@ -51,6 +63,7 @@ alt.themes.enable('latimes')
 
 def main():
 
+    st.title('Agile Metrics for Personal Kanban')
     # Multi-select category filter
     categories = list(df['column_name'].unique())
     
@@ -73,11 +86,38 @@ def main():
         ]
     filter_dates_categories_df = filter_dates_df.query(f'column_name != {filter_categories}')
     
+    filter_dates_df_cfd = df_cfd[
+        (df_cfd['date_day'] >= pd.to_datetime(filter_min_date)) & 
+        (df_cfd['date_day'] <= pd.to_datetime(filter_max_date))
+        ]
+
+    # Stability calc
+    mean_arrival_rate = filter_dates_df_cfd['num_arrivals'].mean()
+    mean_inventory = filter_dates_df_cfd['num_inventory'].mean()
+    mean_lead_time = mean_inventory/mean_arrival_rate
+
+    # Text explanation
+    st.markdown(
+        f"""
+        The Average Lead Time for this system is {mean_lead_time}.
+        """
+    )
+
+    # Lead time
+
+    st.altair_chart(
+        alt.Chart(filter_dates_df_cfd, width=(140*5)).mark_line(color='#5ba3cf').encode(
+            x= 'date_day:T',
+            y= 'avg_lead_time:Q'
+        ).transform_calculate(
+            avg_lead_time='(datum.avg_daily_inventory_past_two_weeks/datum.avg_daily_arrival_past_two_weeks)'
+        )
+    )
+
 
     # WIP Graph
 
-    st.altair_chart(
-        alt.Chart(filter_dates_categories_df, width=(140*5)).mark_area().encode(
+    wip_graph = alt.Chart(filter_dates_categories_df, width=(140*5)).mark_area().encode(
             x= alt.X("date_hour:T", title=""),
             y= alt.Y("count()", title="# of work items by Kanban column"),
             tooltip=[
@@ -92,20 +132,45 @@ def main():
                     domain=['Archived', 'Backlog', 'Analysis', 'Ready for Work', 'Execute', 'Verify', 'Done'],
                     range=['#4d4d4d', '#c4c4c4', '#f87f2c', '#9d9d9d', '#86bcdc', '#3887c0', '#757575']
                 ),
-                title='Kanban column'
+                legend=alt.Legend(orient='bottom',
+                title='Kanban column')
             ),
             order= alt.Order('hierarchy', sort='descending')
         ).properties(
             title='WIP area chart'
-        ).configure_legend(
-            orient='bottom'
         )
+
+    arrival_rate = alt.Chart(filter_dates_df_cfd, width=(140*5)).mark_line(interpolate='step-after', strokeDash=[1,1], color='#a778cb').encode(
+            x='date_day:T',
+            y='num_arrivals'
+        )
+    
+    avg_arrival_rate = arrival_rate.mark_line(strokeDash=[10,1], color='#a778cb').transform_window(
+        rolling_mean = 'mean(num_arrivals)',
+        frame=[-7]
+    ).encode(
+        x='date_day:T',
+        y='rolling_mean:Q'
     )
+
+    inventory = alt.Chart(filter_dates_df_cfd).mark_line(interpolate='step-after', color='#f8608f', strokeDash=[1,1]).encode(
+            x='date_day:T',
+            y='num_inventory'
+        )
+    
+    avg_inventory = inventory.mark_line(strokeDash=[10,1], color='#f8608f').transform_window(
+        rolling_mean = 'mean(num_inventory)',
+        frame=[-7]
+    ).encode(
+        x='date_day:T',
+        y='rolling_mean:Q'
+    )
+    
+    st.altair_chart(wip_graph + arrival_rate + avg_arrival_rate + inventory + avg_inventory)
 
     # CFD Graph
 
-    st.altair_chart(
-        alt.Chart(filter_dates_df, width=(140*5)).mark_area().encode(
+    st.altair_chart(alt.Chart(filter_dates_df, width=(140*5)).mark_area().encode(
             x= alt.X("date_hour:T", title=""),
             y= alt.Y("count()", title="# of work items by Kanban column"),
             tooltip=[
